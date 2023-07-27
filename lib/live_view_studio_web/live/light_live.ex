a0dcb6964b3731da87306bb48d15a82d6973605b
root <root@DESKTOP-DH9FK14.localdomain>
2023-07-27 17:06:19 -0600

    added light slider

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -1,8 +1,9 @@
 defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
+  import LiveViewStudio.Helpers
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, brightness: 10)
+    socket = assign(socket, brightness: 10, slider_value: 50)
     IO.inspect(self(), label: "MOUNT")
     {:ok, socket}
   end
@@ -37,8 +38,25 @@ defmodule LiveViewStudioWeb.LightLive do
           <img src="/images/fire.svg" alt="" />
         </button>
       </div>
+
+      <form phx-change="slider-change">
+        <input
+          type="range"
+          min="0"
+          max="100"
+          name="brightness"
+          value={@slider_value}
+        />
+      </form>
     </div>
     """
+  end
+
+  def handle_event("slider-change", params, socket) do
+    brightness = String.to_integer(params["brightness"])
+    red_map(brightness)
+    socket = assign(socket, brightness: brightness)
+    {:noreply, socket}
   end
 
   def handle_event("random", _, socket) do
